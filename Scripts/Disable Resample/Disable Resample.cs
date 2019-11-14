9e18b892ed09aeae05ecbeeac2fb02e9c2ac3319
Jagerente <47350537+Jagerente@users.noreply.github.com>
2019-11-15 01:51:45 +0300

    Created Disable Resample script

---
+++ Scripts/Disable Resample/Disable Resample.cs
@@ -0,0 +1,18 @@
+﻿using Sony.Vegas;
+
+public class EntryPoint
+{
+    public void FromVegas(Vegas client)
+    {
+        foreach (Track track in client.Project.Tracks)
+        {
+            if (track.IsVideo())
+            {
+                foreach (var events in track.Events)
+                {
+                    ((VideoEvent)events).ResampleMode = VideoResampleMode.Disable;
+                }
+            }
+        }
+    }
+}
